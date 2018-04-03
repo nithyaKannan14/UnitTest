52a8d6c4b3961f1acf796679f67e98d399349903
nithya.a.kannan <nithya.a.kannan@accenture.com>
2018-04-03 16:36:01 +0100

    Initial Commit

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+
+        public string Generate(int value)
+        {
+            if ((value % 3 == 0) && (value % 5 == 0))
+            {
+                string word = "FizzBuzz";
+                return Convert.ToString(word);
+            }       
+            
+            if (value % 5 == 0)
+            {
+                string word = "Buzz";
+                return Convert.ToString(word);
+            }
+          
+           if(value%3!=0 && value % 5!= 0)
+            {
+                return Convert.ToString(value);
+            }
+            else
+            {
+                string word = "Fizz";
+                return Convert.ToString(word);
+            }
+
+        }
+    }
+}
---
+++ FizzBuzzer.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzz;
+
+namespace FizzBuzzer.Tests
+{
+    public class FizzBuzzerTests
+    {
+        FizzBuzz.FizzBuzzer fizzBuzzer;
+
+        public FizzBuzzerTests()
+        {
+            fizzBuzzer = new FizzBuzz.FizzBuzzer();
+
+        }
+
+        [Test]
+        public void ValidateWordFizz()
+        {
+            int inputValue = 3;
+            string returnWord = fizzBuzzer.Generate(inputValue);
+            Assert.AreEqual(returnWord, "Fizz");
+        }
+
+        [Test]
+        public void ValidateWordBuzz()
+        {
+            int inputValue = 5;
+            string returnWord = fizzBuzzer.Generate(inputValue);
+            Assert.AreEqual(returnWord, "Buzz");
+        }
+
+        [Test]
+        public void ValidateWordFizzBuzz()
+        {
+            int inputValue = 15;
+            string returnWord = fizzBuzzer.Generate(inputValue);
+            Assert.AreEqual(returnWord, "FizzBuzz");
+        }
+
+        [Test]
+        public void ValidateInputValueNotDivisible()
+        {
+            int inputValue = 8;
+            string returnWord = fizzBuzzer.Generate(inputValue);
+            if (returnWord.Equals(inputValue.ToString()))
+            {
+                Assert.Pass("inputValue equals to return value");
+            }         
+
+        }
+    }
+}
+
